9bb025df16dccedf953de93c3aa0619ea1aa96ff
Sam_intech <sonowosamuel@icloud.com>
2026-03-01 12:24:23 +0000

    fix: update for CORS for production

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1|0\.0\.0\.0)(:\d+)?$",
+    # allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1|0\.0\.0\.0)(:\d+)?$",
+    allow_origins=[
+      "http://localhost:5173",
+      "http://localhost:8080",
+      "https://ravishing-dream-production.up.railway.app",
+      "https://cassandra-production-516a.up.railway.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
